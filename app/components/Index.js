a163c7c33a81e6b6ea750d6b275574f1fcbf10b3
Matthew Jesuele <mjesuele@gmail.com>
2016-04-01 17:10:11 -0700

    Fixed some stuff up on index page and hopefully now the hero image displays.

--- app/components/Index.js
+++ app/components/Index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Body from './Body';
-import Header from './Header';
-import './Index.scss';
 import {
   Code,
   Footer, FooterAddress,
@@ -14,18 +11,22 @@ import {
   SignupInline, SignupModal,
   Team,
   TeamMember,
-} from "neal-react";
+} from 'neal-react';
+import Body from './Body';
+import Header from './Header';
+import heroImg from '../../public/img/hero-bg-01.jpg';
+import './Index.scss';
 
-//Specify the company brand
-const brandName = "dev.js";
+// Specify the company brand
+const brandName = 'dev.js';
 const brand = <span>{brandName}</span>;
 
-//Specify company address
+// Specify company address
 const businessAddress = (
   <address>
-    <strong>{brandName}</strong><br/>
-    735 S. Saint Andrews Pl<br/>
-    Los Angeles, CA 90005<br/>
+    <strong>{brandName}</strong><br />
+    735 S. Saint Andrews Pl<br />
+    Los Angeles, CA 90005<br />
     +1 213 555 5555
   </address>
 );
@@ -38,16 +39,19 @@ export default () => (
         dev.js
       </Header>
 
-      <Hero backgroundImage="hero-bg-01.jpg">
-          <h1 className="display-4"> Dev.js crafts minimal viable products and integrations with the latest Javascript technologies. </h1>
+      <Hero backgroundImage={heroImg}>
+        <h1 className="display-4">
+          Dev.js crafts minimal viable products and integrations with the latest Javascript technologies.
+        </h1>
       </Hero>
 
       <Body>
         This is the body.
       </Body>
-      <Footer brandName={brandName}
-        address={businessAddress}>
-      </Footer>
+      <Footer
+        address={businessAddress}
+        brandName={brandName}
+      />
     </Page>
   </div>
 );
