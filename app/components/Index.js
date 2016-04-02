d2372b2c2cd8bb41bb08aa61317e609341625f3d
meganmcgeee <meganpmcgee@gmail.com>
2016-04-01 18:05:10 -0700

    added sub hero class to contain portfolio links

--- app/components/Index.js
+++ app/components/Index.js
@@ -45,15 +45,20 @@ export default () => (
         </h1>
       </Hero>
 
+      <Section className="subhero">
+        <ImageList centered>
+          <ImageListItem src="img/press/cnn-logo.png" url="http://www.cnn.com"/>
+          <ImageListItem src="img/press/forbes-logo.png" url="http://forbes.com/"/>
+          <ImageListItem src="img/press/theverge-logo.png" url="http://www.theverge.com/"/>
+        </ImageList>
+      </Section>
+
       <Section>
        <Team>
-         <TeamMember name="Member 1" title="Co-founder" imageUrl="img/people/grumpycat.jpg">
+         <TeamMember name="Matthew Jesuele" title="Co-founder" imageUrl="img/people/grumpycat.jpg">
          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
          </TeamMember>
-         <TeamMember name="Member 2" title="Co-founder" imageUrl="img/people/boo.jpg">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-         </TeamMember>
-         <TeamMember name="Member 3" title="Co-founder" imageUrl="img/people/panda.jpg">
+         <TeamMember name="Megan McGee" title="Co-founder" imageUrl="img/people/boo.jpg">
          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
          </TeamMember>
        </Team>
